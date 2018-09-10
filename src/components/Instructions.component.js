8ff14455d25384a13440acd104009488cc4956fd
juliengrima <gogrinformatique@gmail.com>
2018-09-10 14:19:07 +0200

    modi amelior code

--- src/components/Instructions.component.js
+++ src/components/Instructions.component.js
@@ -9,25 +9,26 @@ class Instructions extends Component {
     }
   }
 
-  render() {
+  renderImage() {
     if (this.state.report === null) {
-      return (
-        <div className="instructions">
-          Follow the instructions on the README to get started!
-        </div>
-      )
     } else {
       return (
-        <div className="instructions">
-          Follow the instructions on the README to get started!
-          <img
-            className="instructions__logo"
-            src="https://uiuc.hack4impact.org/img/colored-logo.png"
-            alt="h4i logo"
-          />
-        </div>
+        <img
+          className="instructions__logo"
+          src="https://uiuc.hack4impact.org/img/colored-logo.png"
+          alt="h4i logo"
+        />
       )
     }
+  }
+
+  render() {
+    return (
+      <div className="instructions">
+        Follow the instructions on the README to get started!
+        {this.renderImage()}
+      </div>
+    )
   }
 }
 
