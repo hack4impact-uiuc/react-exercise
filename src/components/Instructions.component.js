3558b3a52439a84aecac94139d6b0bc750762f3e
juliengrima <gogrinformatique@gmail.com>
2018-09-10 13:59:18 +0200

    Part 1 ok

--- src/components/Instructions.component.js
+++ src/components/Instructions.component.js
@@ -2,17 +2,32 @@ import React, { Component } from 'react'
 import './../styles/instructions.css'
 
 class Instructions extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      report: true
+    }
+  }
+
   render() {
-    return (
-      <div className="instructions">
-        Follow the instructions on the README to get started!
-        <img
-          className="instructions__logo"
-          src="https://uiuc.hack4impact.org/img/colored-logo.png"
-          alt="h4i logo"
-        />
-      </div>
-    )
+    if (this.state.report === null) {
+      return (
+        <div className="instructions">
+          Follow the instructions on the README to get started!
+        </div>
+      )
+    } else {
+      return (
+        <div className="instructions">
+          Follow the instructions on the README to get started!
+          <img
+            className="instructions__logo"
+            src="https://uiuc.hack4impact.org/img/colored-logo.png"
+            alt="h4i logo"
+          />
+        </div>
+      )
+    }
   }
 }
 
