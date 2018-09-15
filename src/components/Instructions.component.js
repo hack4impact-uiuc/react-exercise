3d8b4ac11cb0265ea3d507e15a521c2d044fcf75
Neeraj Aggarwal <aggarwal.neeraj141@gmail.com>
2018-09-14 20:30:10 -0500

    Complete part 1

--- src/components/Instructions.component.js
+++ src/components/Instructions.component.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react'
 import './../styles/instructions.css'
 
 class Instructions extends Component {
-  render() {
-    return (
-      <div className="instructions">
-        Follow the instructions on the README to get started!
+  renderImage() {
+    if (this.props.displayImage) {
+      return (
         <img
           className="instructions__logo"
           src="https://uiuc.hack4impact.org/img/colored-logo.png"
           alt="h4i logo"
         />
-      </div>
-    )
+      )
+    } else {
+      return null
+    }
+  }
+
+  render() {
+    return <div className="instructions">{this.renderImage()}</div>
   }
 }
 
